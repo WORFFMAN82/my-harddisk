7da5c32ed23d9a3a4463b830284b576908b3a337
WORFFMAN82 <stump38@email.com>
2026-01-18 21:16:44 +0900

    fix CSV with proper header and UTF-8 encoding

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4259515396"
+    serviceWorkerVersion: "1408046462"
   }
 });
