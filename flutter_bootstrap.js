444abee222b6216bd99a2f9cc9cfff3137293fe6
WORFFMAN82 <stump38@email.com>
2026-01-18 22:44:59 +0900

    add 20 real products from Excel

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "588319263"
+    serviceWorkerVersion: "3184100851"
   }
 });
