8fb0753b04f8dd1cf1e425fba87bf4c2c3899db2
WORFFMAN82 <stump38@email.com>
2026-01-18 18:04:33 +0900

    add reverse margin logic

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3639091239"
+    serviceWorkerVersion: "2889534084"
   }
 });
