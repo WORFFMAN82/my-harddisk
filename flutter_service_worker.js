b7935ebda1b5d4967958a9c5d736d08df5fa6f9b
WORFFMAN82 <stump38@email.com>
2026-01-25 23:37:59 +0900

    fix: 웹에서 CSV 다운로드 기능 수정

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "c3443b3942f439c627d0734174a889a9
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "dc681b2d80b058fca7381ac031326299",
+"flutter_bootstrap.js": "7d8cb40e79cfe21e0b5a9e0169a60643",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
